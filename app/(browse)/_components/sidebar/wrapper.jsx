d5bb89ec89403e789657f5dcf25b69e1d14e0439
AmitGangani <amitgangani2814@gmail.com>
2024-05-27 14:29:04 +0530

    added creator dashboard

--- app/(browse)/_components/sidebar/wrapper.jsx
+++ app/(browse)/_components/sidebar/wrapper.jsx
@@ -7,8 +7,8 @@ import { useSidebar } from "@/store/use-sidebar";
 
 import { ToggleSkeleton } from "./toggle";
 import { useEffect, useState } from "react";
-// import { FollowingSkeleton } from "./following";
-// import { RecommendedSkeleton } from "./recommended";
+import { FollowingSkeleton } from "./following";
+import { RecommendedSkeleton } from "./recommended";
 
 export const Wrapper = ({ children }) => {
    // const isClient = useIsClient();
@@ -24,8 +24,8 @@ export const Wrapper = ({ children }) => {
       return (
          <aside className="fixed left-0 flex flex-col w-[70px] lg:w-60 h-full bg-background border-r border-[#2D2E35] z-50">
             <ToggleSkeleton />
-            {/* <FollowingSkeleton /> 
-       <RecommendedSkeleton />  */}
+            <FollowingSkeleton />
+            <RecommendedSkeleton />
          </aside>
       );
    }
